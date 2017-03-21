b92c53ad121b050d86509429b2afe6705391a939
Simon A. Jensen <pc.manden@gmail.com>
2017-03-21 20:38:55 +0100

    Added Core framework - init

---
+++ FPCore/FPCore/FPCore.h
@@ -0,0 +1,19 @@
+//
+//  FPCore.h
+//  FPCore
+//
+//  Created by Simon Jensen on 21/03/17.
+//  Copyright © 2017 Askilada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FPCore.
+FOUNDATION_EXPORT double FPCoreVersionNumber;
+
+//! Project version string for FPCore.
+FOUNDATION_EXPORT const unsigned char FPCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FPCore/PublicHeader.h>
+
+
---
+++ FPCore/FPCore/FPCore.swift
@@ -0,0 +1,22 @@
+//
+//  FPCore.swift
+//  FPCore
+//
+//  Created by Simon Jensen on 21/03/17.
+//  Copyright © 2017 Askilada. All rights reserved.
+//
+
+import Foundation
+
+
+public class FPCore {
+    public static let shared: FPCore = FPCore();
+    let apiKey = ""
+    let apiSecret = ""
+    
+    var authToken:String?
+    
+    
+    
+    
+}
