472a161a60372bda2d1035ba950d45a457ef445a
Simon A. Jensen <pc.manden@gmail.com>
2017-04-02 21:05:14 +0200

    Config file added for Flickr

---
+++ FPCore/FPCore/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  FPCore
+//
+//  Created by Simon Jensen on 22/03/17.
+//  Copyright © 2017 Askilada. All rights reserved.
+//
+
+import Foundation
+
+struct FPConfig {
+    static let apiKey = ""
+    static let apiSecret = ""
+}
